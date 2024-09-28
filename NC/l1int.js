fa88062dc62cc8cd0b96114813b2e8bcd9da0c8e
Mandeep Kalra <mkmandeepkalra@gmail.com>
2024-09-28 16:08:58 +0530

    Create l1int.js

---
+++ NC/l1int.js
@@ -0,0 +1,29 @@
+// Task 01: calculate the number of identical elements
+// You need to output the number of repetitions of elements for each element to the console.
+const obj1 = {
+  1: 'a',
+  2: 'b',
+  3: 'a',
+  4: 'c',
+  5: 'a',
+  6: 'd',
+  7: 'c',
+};
+// for this object - { a: 3, b:  1, c: 2, d: 1 }
+
+
+
+// Task 02 : Increment elements that are larger than the previous one in the original array
+const ar = [4, 5, 6, 7, 8, 9, 2, 0, 3, 0];
+
+
+
+// Task 03 : sort based on firstname & dept.name
+const employees = [
+  { firstName: 'John', lastName: 'Doe', dept: { id: 1, name: 'nn' } },
+  { firstName: 'Anna', lastName: 'Smith', dept: { id: 2, name: 'ff' } },
+  { firstName: 'Peter', lastName: 'Jones', dept: { id: 3, name: 'ss' } },
+  { firstName: 'John', lastName: 'Hue', dept: { id: 4, name: 'aa' } },
+];
+
+// console.log(employees);
